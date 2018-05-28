c2780586eb389b4fc180380122419350fc93da1e
Alexey Zubov <alexzoo@Alexeys-MacBook-Pro.local>
2018-05-28 22:29:49 +0300

    fix maven dep

--- src/test/java/testng_temp.java
+++ src/test/java/testng_temp.java
@@ -4,11 +4,11 @@ public class testng_temp {
 
     @Test
     public void Test1(){
-        System.out.println("Hello");
+        System.out.println("Test One");
     }
 
     @Test
     public void Test2() {
-        System.out.println("Bye");
+        System.out.println("Test Two");
     }
 }
