76696cb9eb097d1833cf8dcdf822c560fba05df4
Alexey Zubov <alexzoo@Alexeys-MacBook-Pro.local>
2018-06-04 20:23:34 +0300

    add apache POI

--- src/test/java/zu/testselenium/MyTestNGTests.java
+++ src/test/java/zu/testselenium/MyTestNGTests.java
@@ -1,5 +1,8 @@
 package zu.testselenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
@@ -8,15 +11,37 @@ import java.util.Properties;
 
 public class MyTestNGTests {
 
+    WebDriver driver;
+
     @Test
     public void userTest() throws IOException {
+
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver 2");
+        System.setProperty("webdriver.gecko.driver", "drivers/geckodriver 2");
+
 
         Properties prop = new Properties();
         FileInputStream fis = new FileInputStream("/Users/alexzoo/Documents/projects/pageobjectseleniumtest/src/test/java/zu/testselenium/datadriventest.properties");
 
         prop.load(fis);
 
-        System.out.println(prop.getProperty("user"));
+        if (prop.getProperty("browser").contains("chrome")){
+
+            driver = new ChromeDriver();
+            System.out.println("Chrome browser");
+
+        } else {
+
+            driver = new FirefoxDriver();
+            System.out.println("Firefox browser");
+        }
+
+
+        System.out.println(prop.getProperty("user test"));
+
+        driver.get(prop.getProperty("url"));
+        //driver.quit();
+
     }
 
     @Test
@@ -27,6 +52,6 @@ public class MyTestNGTests {
 
         prop.load(fis);
 
-        System.out.println(prop.getProperty("password"));
+        System.out.println(prop.getProperty("password test"));
     }
 }
