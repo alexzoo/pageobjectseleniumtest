45e009b677ae15d638b9cfbcdf8851d158e3af9d
Alexey Zubov <alexzoo@Alexeys-MacBook-Pro.local>
2018-06-04 18:50:02 +0300

    add datadriven test

--- src/test/java/zu/testselenium/MyTestNGTests.java
+++ src/test/java/zu/testselenium/MyTestNGTests.java
@@ -2,15 +2,31 @@ package zu.testselenium;
 
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class MyTestNGTests {
 
     @Test
-    public void Test1(){
-        System.out.println("Test One");
+    public void userTest() throws IOException {
+
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream("/Users/alexzoo/Documents/projects/pageobjectseleniumtest/src/test/java/zu/testselenium/datadriventest.properties");
+
+        prop.load(fis);
+
+        System.out.println(prop.getProperty("user"));
     }
 
     @Test
-    public void Test2() {
-        System.out.println("Test Two");
+    public void passwordTest() throws IOException {
+
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream("/Users/alexzoo/Documents/projects/pageobjectseleniumtest/src/test/java/zu/testselenium/datadriventest.properties");
+
+        prop.load(fis);
+
+        System.out.println(prop.getProperty("password"));
     }
 }
