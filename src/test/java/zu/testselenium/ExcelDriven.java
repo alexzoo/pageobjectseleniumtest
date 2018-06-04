38b02847f25a1a012966c0e493b34132ec2fc0b3
Alexey Zubov <alexzoo@Alexeys-MacBook-Pro.local>
2018-06-04 22:08:17 +0300

    excel DD

--- src/test/java/zu/testselenium/ExcelDriven.java
+++ src/test/java/zu/testselenium/ExcelDriven.java
@@ -12,22 +12,18 @@ public class ExcelDriven {
 
     public static void main(String[] args) throws IOException {
 
-        FileInputStream file = new FileInputStream("/Users/alexzoo/Documents/projects/pageobjectseleniumtest/data.xls");
+        FileInputStream file = new FileInputStream("/Users/alexzoo/Documents/projects/pageobjectseleniumtest/data2.xlsx");
+
 
         XSSFWorkbook wb = new XSSFWorkbook(file);
-        XSSFSheet sheet = wb.getSheet("script");
+        XSSFSheet sheet = wb.getSheet("Sheet1");
         XSSFRow row = sheet.getRow(2);
         XSSFCell cell = row.getCell(2);
         String value = cell.getStringCellValue();
 
+        System.out.println(value);
         
-
-
-
-
-
-
-
+        
 
 
     }
